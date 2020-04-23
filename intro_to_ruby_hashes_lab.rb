d77c6c0b3ba08f35ac119a4e7ef11d34f8257e6d
Lon Bitner <lonbitner@gmail.com>
2020-04-23 18:16:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,20 +2,24 @@ def new_hash
   new_hash = {}
 end
 
+def new_hash
+	new_hash = {}
+end
+
 def actor
-  actor = {name: "Dwayne The Rock Johnson"}
+	actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {
-	  :railroads => {}
+		:railroads => {}
 	}
 
 end
 
-def monopoly
+def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {pieces: 4, names: {}, rent_in_dollars: {}}
+		:railroads => {pieces: 4, names: {}, rent_in_dollars: {}}
 	}
 
 end
